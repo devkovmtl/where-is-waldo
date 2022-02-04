4f1b74b70dcb266a9a6ebd7aa327b2bb18af51b3
Mikael <dev.kov.mtl@gmail.com>
2022-02-04 10:57:38 -0500

    can read the position of mouse

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+type MousePosition = {
+  top: number;
+  left: number;
+};
+
 export default function Game() {
+  const [mousePosition, setMousePosition] = useState<MousePosition>({
+    top: 0,
+    left: 0,
+  });
   const params = useParams();
   const levelId = params.levelId;
+
+  const handleImgClick = (e: React.MouseEvent) => {
+    const rect = (e.target as Element).getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    setMousePosition({ top: y, left: x });
+  };
+
   return (
-    <div className='mt-20 container mx-auto p-5 min-w-[640px]'>
+    <div className='mt-20 container mx-auto p-5 w-[640px]'>
       <img
         className='w-full hover:cursor-crosshair'
         src={require(`../../images/levels/Level ${levelId}.jpg`)}
         alt="where's waldo"
+        onClick={handleImgClick}
       />
     </div>
   );
