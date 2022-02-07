8fed986c26273874d8361528e43fb7f736d62f24
Mikael <dev.kov.mtl@gmail.com>
2022-02-07 17:47:03 -0500

    Remove old apikey

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCGBqcjAtw56ZL1NQijGBr5_uLhlxOHJiA',
-  authDomain: 'where-s-waldo-c516e.firebaseapp.com',
-  projectId: 'where-s-waldo-c516e',
-  storageBucket: 'where-s-waldo-c516e.appspot.com',
-  messagingSenderId: '548163644124',
-  appId: '1:548163644124:web:7d3622c95c2d027e172219',
+  apiKey: '<APIKEY>',
+  authDomain: '<APIKEY>',
+  projectId: '<APIKEY>',
+  storageBucket: '<APIKEY>',
+  messagingSenderId: '<APIKEY>',
+  appId: '<APIKEY>',
 };
 
 const app = initializeApp(firebaseConfig);
